54cac0877e0bbf354961605dbaaa073c2b647eb3
Muhammad Hisham <hisham.cs.engineer@gmail.com>
2024-07-11 00:17:41 +0530

    Dummy data generator question

---
+++ 97-EX-16-Dummy data generator/main.js
@@ -0,0 +1,12 @@
+// Dummy data generator in this format in a collection called employees in a db called company
+// {
+//     name:"Harry",
+//     salary:4500,
+//     position:"developer",
+//     city:"New york",
+//     language:"Python"
+// }
+
+// Generate 10 such records when a button called Generate data is clicked!
+// Create an Express app with mongoose to acheive it
+// Everytime the button is clicked, you should clear the collection 
