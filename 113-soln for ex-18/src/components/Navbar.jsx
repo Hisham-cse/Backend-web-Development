502f48e0e656ce7189c37c17d7470018b1fe971a
Muhammad Hisham <hisham.cs.engineer@gmail.com>
2024-08-31 10:17:12 +0530

    113-soln Ex-18

---
+++ 113-soln for ex-18/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>AboutUs</li>
+            <li>ContactUs</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
